0cc1cac71890f09b0603e64ef5b33c4c0638b0d5
RuanCunha <ruanvcunha@hotmail.com>
2022-06-23 14:05:27 -0300

    [fix] failed login return correct html status

--- app/backend/src/middlewares/loginCheck.middleware.ts
+++ app/backend/src/middlewares/loginCheck.middleware.ts
@@ -3,7 +3,9 @@ import { Response, Request, NextFunction } from 'express';
 const checkLogInfo = (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
 
-  if (!email || !password) return res.status(401).json({ message: 'All fields must be filled' });
+  if (email === '' || password === '') {
+    return res.status(400).json({ message: 'All fields must be filled' });
+  }
 
   next();
 };
